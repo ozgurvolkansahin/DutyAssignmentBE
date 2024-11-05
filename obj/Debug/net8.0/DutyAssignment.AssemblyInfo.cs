fe6f00d7be582e384587f194d1617cd85c2927ef
Ozgur Volkan Sahin <ozgurvolkansahin@hotmail.com>
2024-11-05 08:46:45 +0300

    gitignore update

--- obj/Debug/net8.0/DutyAssignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/DutyAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DutyAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+89ce877cdc26cafa7e76a5c9589a2d23b14e5a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+2da1a5f9d6165755e4f1423048b057b2921c4554")]
 [assembly: System.Reflection.AssemblyProductAttribute("DutyAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DutyAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
